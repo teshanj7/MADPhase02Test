9b0adf99c46d2d51e97700ba3c0b55e75964ef3d
IT21343216 <it21343216@my.sliit.lk>
2023-04-29 13:25:13 +0530

    Login commit done

---
+++ app/src/main/java/com/example/pawningsystem/index.kt
@@ -0,0 +1,11 @@
+package com.example.loginpage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class index : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_index2)
+    }
+}
